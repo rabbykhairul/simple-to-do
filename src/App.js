a4e98ea59a4310f60c066073b82bbd488ffd3bd8
rabbykhairul <rabbykhairul@gmail.com>
2020-12-01 22:59:25 +0600

    render to do list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <h1>Hello world!</h1>;
+import React, { Component } from "react";
+import ListTable from "./components/ListTable";
+
+class App extends Component {
+  state = {
+    toDoItems: [
+      { id: 1, content: "Create a to do list" },
+      { id: 2, content: "abc" },
+      { id: 3, content: "Ct" },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <ListTable
+          items={this.state.toDoItems}
+          onUpdate={this.updateItem}
+          onDelete={this.deleteItem}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
